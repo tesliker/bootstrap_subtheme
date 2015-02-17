6e48592d7f44aaa0c882d91680cd947aba055521
tesliker <tesliker@sliker.com>
2015-02-17 11:18:26 -0500

    first commit

---
+++ js/fourstar.js
@@ -0,0 +1,6 @@
+/*(function ($) {
+$(document).ready(function () {
+$('.carousel-caption').hide(500);
+
+});
+})(jQuery); */
---
+++ templates/ds-2col-stacked--node-article.tpl.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 2 column stacked template.
+ */
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="ds-2col-stacked <?php print $classes;?> clearfix">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <<?php print $header_wrapper ?> class="<?php print $header_classes; ?>">
+    <?php print $header; ?>
+  </<?php print $header_wrapper ?>>
+<?php if ($content['field_media_type']['#items']['0']['tid'] !== '8') { ?>
+  <<?php print $left_wrapper ?> class="<?php print $left_classes; ?>">
+    <?php //print $left; 
+	
+	print render($content['field_image']); 
+	
+	?>
+  </<?php print $left_wrapper ?>>
+  <?php } ?>
+
+  <<?php print $right_wrapper ?> class="<?php print $right_classes; ?>">
+    <?php print $right; ?>
+  </<?php print $right_wrapper ?>>
+
+  <<?php print $footer_wrapper ?> class="<?php print $footer_classes; ?>">
+    <?php print $footer; ?>
+  </<?php print $footer_wrapper ?>>
+
+</<?php print $layout_wrapper ?>>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
