76c79ed1091ae175af339d9200be1e8e535742ff
Joe Price <pricejn2@gmail.com>
2017-11-04 22:29:14 -0400

    add hoverintent library and initial script

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,39 @@
   };
 
 })(jQuery, Drupal);
+
+(function ($) {
+  $(document).ready(function () {
+
+    $('.top-cart-title').each(function(){
+
+      var $link = $(this),
+          $div = $('#block-commerce-cart-cart'),
+          div_or_link_is_hovered = false,
+          show = function(){
+             $div.slideDown(600);
+             $link.addClass('active');
+          },
+          hide = function(){
+              setTimeout(function(){
+                  if(div_or_link_is_hovered === false){
+                      $div.slideUp(600);
+                      $link.removeClass('active');
+                  };
+              }, 300);
+          };
+
+        $link.add($div).mouseover(function(){
+            div_or_link_is_hovered = true;
+            show();
+        });
+
+        $link.add($div).mouseout(function(){
+            div_or_link_is_hovered = false;
+            hide();
+        });
+
+    });
+
+  });
+})(jQuery);
